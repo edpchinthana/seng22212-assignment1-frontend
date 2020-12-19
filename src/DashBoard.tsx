931bd572f2c7f09cb166daf5963653805f1fdb33
PadmaGnanapriya <padmaisuru@gmail.com>
2020-12-19 07:30:43 +0530

    Add chart js

--- src/DashBoard.tsx
+++ src/DashBoard.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Line } from "react-chartjs-2";
 import {
     useParams
 } from "react-router-dom";
+import {Container} from "react-bootstrap";
 
 const DashBoard: React.FC = () => {
     let {sensor}: any = useParams();
@@ -20,11 +22,26 @@ const DashBoard: React.FC = () => {
         }
     }
 
+    const data = {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+        datasets: [
+            {
+                label: "Second dataset",
+                data: [33, 25, 35, 51, 54, 76],
+                fill: false,
+                borderColor: "#742774"
+            }
+        ]
+    };
+
     return (
         <React.Fragment>
             <h2 className='pt-4'>Dashboard</h2>
             <p>{sensor}</p>
             {renderIcon()}
+            <Container>
+                <Line data={data}/>
+            </Container>
 
 
         </React.Fragment>
