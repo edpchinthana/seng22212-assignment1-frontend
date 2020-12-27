de513cf055fe51345fc759b754262ff19262766d
wvGithmi <wvganjana@gmail.com>
2020-12-27 14:20:54 +0530

    Added alert-history UI

--- src/pages/AlertHistory.tsx
+++ src/pages/AlertHistory.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown'
 
 const AlertHistory: React.FC = () => {
     document.title = 'weatherApp | alert history'
@@ -6,6 +7,60 @@ const AlertHistory: React.FC = () => {
         <div  className='min-vh-100'>
             <br/><br/>
             <h2 className='pt-4'>Alert History</h2>
+            <div className="App">
+                <div className="container">
+                    <Dropdown className="mt-5 text-left">
+                        <Dropdown.Toggle className="btn btn-dark text-break text-dark bg-white border rounded-0 border-dark shadow-none" variant="success" id="dropdown-basic">
+                            Sensors
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#">First Item</Dropdown.Item>
+                            <Dropdown.Item href="#">Second Item</Dropdown.Item>
+                            <Dropdown.Item href="#">Third Item</Dropdown.Item>
+                            <Dropdown.Item href="#">Fourth Item</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+                    <div className="table-bordered text-monospace text-break text-center text-black-50 mt-5">
+                        <table className="table table-striped table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Sensor ID</th>
+                                    <th>Threshold Level</th>
+                                    <th>Sensor Value</th>
+                                    <th>Date &amp; Time</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>ID1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>ID2</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>ID3</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>ID4</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
