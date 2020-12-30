8ed3ecd05c661d368b51bb0491e139405218e703
supuniuthpala <mdsupuniuthpala@gmail.com>
2020-12-29 21:14:37 -0800

    commit

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -49,6 +49,20 @@ const Settings: React.FC = () => {
             <br/><br/><br/>
             <h2>Settings</h2>
             <br/>
+
+            <div className="form-group">
+                <h1 style={{fontSize: '25px', padding: '5px 10px', textAlign: 'left'}}>Change Threshold Value</h1>
+            </div>
+            <br/>
+            <select style={{paddingBottom: '0px', marginBottom: '20px', marginLeft: '10px', width: '200px', height: '38px', float: 'left'}}>
+                <option selected>Temperature</option>
+                <option>Humidity</option>
+                <option>Pressure </option>
+            </select>
+
+
+
+
             <Button className='p-button mt-3' onClick={handleSignOut}>Sign out</Button>
         </div>
     );
