6e972ba77a0acb87580cb6159ace33e5eaef250d
hansikaherath <kavihanhe111@gmail.com>
2020-12-30 20:16:06 +0530

    protectedrouthers

---
+++ src/components/ProtectedRoutes.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Route, Redirect} from 'react-router-dom';
+import {Switch} from 'react-router';
+import Dashboard from "./pages/Dashboard";
+import NavBar from "./common/NavBar";
+import AlertHistory from "./pages/AlertHistory";
+
+function ProtectedRoutes(){
+
+
+    return(
+<div id="page-top">
+    <div id="wrapper">
+        <NavBar/>
+        <Switch>
+            <Route
+                exact
+                path={"/dashboard"}
+                component={Dashboard}
+            />
+            <Route
+                exact
+                path={'/alertHistory'}
+                component={AlertHistory}
+                />
+            <Redirect
+                to={"/dashboard"}
+            />
+        </Switch>
+    </div>
+</div>
+    )
+}
+
+export default ProtectedRoutes;
