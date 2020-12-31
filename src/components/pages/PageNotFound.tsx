ec3d73260e144b948977249d96b6ed91cbeea787
PadmaGnanapriya <padmaisuru@gmail.com>
2020-12-31 15:18:41 +0530

    create 404 page

--- src/components/pages/PageNotFound.tsx
+++ src/components/pages/PageNotFound.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 
 function PageNotFound(){
     return(
-        <div>Not found</div>
+            <div className="main-box">
+                <div className="err">4</div>
+                <i className="far fa-question-circle fa-spin"/>
+                <div className="err2">4</div>
+                <div className="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in
+                    the first place?<p>Let's go
+                        <Link to='/dashboard' className='links'>Dashboard</Link>
+                         and try from there.</p></div>
+            </div>
+
     )
 }
 
