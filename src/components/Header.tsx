eb1115086210fa06bba384ba6dc5d3ab6ef0838d
PadmaGnanapriya <padmaisuru@gmail.com>
2020-12-31 22:19:16 +0530

    Remove dashboard link and set link into monitor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,11 +8,10 @@ const Header: React.FC = () => {
     return (
   <React.Fragment>
       <Navbar className='p-header-area' collapseOnSelect expand="lg" bg="dark" variant="dark" style={{position:'fixed', width: '100%'}}>
-          <Navbar.Brand><Link to='/' className='links'>Monitor</Link></Navbar.Brand>
+          <Navbar.Brand><Link to='/dashboard' className='links'>Monitor</Link></Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
           <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto">
-                  <Nav.Link><Link to='/dashboard' className='links'>Dashboard</Link></Nav.Link>
                   <Nav.Link><Link to='/about' className='links'>About</Link></Nav.Link>
                   <Nav.Link><Link to='/alertHistory' className='links'>Alert History</Link></Nav.Link>
                   <Nav.Link><Link to='/alertSubscribers' className='links'>Alert Subscribers</Link></Nav.Link>
@@ -20,8 +19,7 @@ const Header: React.FC = () => {
                   <Nav.Link><Link to='/members'className='links'>Members</Link></Nav.Link>
               </Nav>
               <Nav>
-                  <Nav.Link><Link to='/settings'className='links'>Settings</Link></Nav.Link>
-                  <Nav.Link><Button onClick={()=> signOutUser()}>Sign out</Button></Nav.Link>
+                  <Nav.Link><Button variant='secondary' onClick={()=> signOutUser()}>Sign out</Button></Nav.Link>
               </Nav>
           </Navbar.Collapse>
       </Navbar>
