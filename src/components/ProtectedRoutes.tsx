dab63d7b9d98fefadf68b968b9ad489b808f646b
PadmaGnanapriya <padmaisuru@gmail.com>
2020-12-31 12:31:19 +0530

    add members page into routing.

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -1,32 +1,42 @@
 import React from 'react';
-import {Route, Redirect} from 'react-router-dom';
+import {Route, Redirect, Link} from 'react-router-dom';
 import {Switch} from 'react-router';
 import Dashboard from "./pages/DashBoard";
 import AlertHistory from "./pages/AlertHistory";
 import Header from './Header';
+import {Nav} from "react-bootstrap";
+import Members from "./pages/Members";
+import Settings from "./pages/Settings";
 
-function ProtectedRoutes(){
+function ProtectedRoutes() {
 
 
-    return(
-<div id="page-top">
-   <Header/>
-    <div id="wrapper" className="pt-5">
-        <Switch>
-            <Route
-                exact
-                path={"/dashboard"}
-                component={Dashboard}
-            />
-            <Route
-                exact
-                path={'/alertHistory'}
-                component={AlertHistory}
-                />
-            
-        </Switch>
-    </div>
-</div>
+    return (
+        <div id="page-top">
+            <Header/>
+            <div id="wrapper" className="pt-5">
+                <Switch>
+                    <Route
+                        exact
+                        path={"/dashboard"}
+                        component={Dashboard}
+                    />
+                    <Route
+                        exact
+                        path={'/alertHistory'}
+                        component={AlertHistory}
+                    />
+                    <Route
+                        exact
+                        path={'/members'}
+                        component={Members}/>
+                    <Route
+                        exact
+                        path={'/settings'}
+                        component={Settings}/>
+                </Switch>
+            </div>
+        </div>
     )
 }
 
@@ -34,4 +44,5 @@ export default ProtectedRoutes;
 
 {/* <Redirect
                 to={"/dashboard"}
-            /> */}
+            /> */
+}
