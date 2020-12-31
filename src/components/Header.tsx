1905e715d3bf36e6c2a605ac7dd4a1d9284d6b30
PadmaGnanapriya <padmaisuru@gmail.com>
2020-12-31 12:25:36 +0530

    Change header with protected routes.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,54 +3,30 @@ import {Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 const Header: React.FC = () => {
-    const getIsLoggedIn = () => localStorage.getItem('IS_LOGGED_IN') === 'true';
-    const sensorCategories = JSON.parse(localStorage.getItem("SensorCategories") as string);
-    ;
+    const sensorCategories = JSON.parse(localStorage.getItem("SensorCategories") as string) || '';
     console.log(sensorCategories)
     const dropdown: string =  "Temperature";
-    // const[dropdown, setDropdown] = useState(sensorCategories[0] || "Temperature");
 
-    const showHeader =() => {
-        if(!getIsLoggedIn()){
-            return  (
-                <Navbar className='p-header-area' collapseOnSelect expand="lg" bg="dark" variant="dark" style={{position:'fixed', width: '100%'}}>
-                    <Navbar.Brand><Link to='/' className='links'>Monitor</Link></Navbar.Brand>
-                    <Nav.Link><Link to='/signin' className='links'>Sign in</Link></Nav.Link>
-
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                </Navbar>
-            )
-        }else {
-            return(
-                <Navbar className='p-header-area' collapseOnSelect expand="lg" bg="dark" variant="dark" style={{position:'fixed', width: '100%'}}>
-                    <Navbar.Brand><Link to='/' className='links'>Monitor</Link></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-
-                            <Nav.Link><Link to='/about' className='links'>About</Link></Nav.Link>
-                            <Nav.Link><Link to='/alert-history' className='links'>Alert History</Link></Nav.Link>
-                            <Nav.Link><Link to='/signin' className='links'>Sign in</Link></Nav.Link>
-                            <NavDropdown title={dropdown}  id="collasible-nav-dropdown">
-                                {
-                                    sensorCategories.map((sensorCategory:string, index:number)=>
-                                        <NavDropdown.Item><Nav.Link><Link key={index} to={`/dashboard/${sensorCategory.toLowerCase()}`}>{sensorCategory}</Link></Nav.Link></NavDropdown.Item>)
-                                }
-                            </NavDropdown>
-                            <Nav.Link><Link to='/members'className='links'>Members</Link></Nav.Link>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link><Link to='/settings'className='links'>Settings</Link></Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            )
-        }
-    }
 
     return (
   <React.Fragment>
-      {showHeader()}
+      <Navbar className='p-header-area' collapseOnSelect expand="lg" bg="dark" variant="dark" style={{position:'fixed', width: '100%'}}>
+          <Navbar.Brand><Link to='/' className='links'>Monitor</Link></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+          <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+
+                  <Nav.Link><Link to='/about' className='links'>About</Link></Nav.Link>
+                  <Nav.Link><Link to='/alert-history' className='links'>Alert History</Link></Nav.Link>
+                  <Nav.Link><Link to='/signin' className='links'>Sign in</Link></Nav.Link>
+                  <Nav.Link><Link to='/dashboard' className='links'>Dashboard</Link></Nav.Link>
+                  <Nav.Link><Link to='/members'className='links'>Members</Link></Nav.Link>
+              </Nav>
+              <Nav>
+                  <Nav.Link><Link to='/settings'className='links'>Settings</Link></Nav.Link>
+              </Nav>
+          </Navbar.Collapse>
+      </Navbar>
   </React.Fragment>
  );}
 
