b234a175f6fa38946abf1ffd38fd93a959437202
wvGithmi <wvganjana@gmail.com>
2020-12-31 15:45:12 +0530

    Get alert subscribers

---
+++ src/repositories/alertSubscribersRepository.ts
@@ -0,0 +1,19 @@
+import RestHTTP from "../services/RestHTTP";
+import { fetchAlertSubscribers } from "../store/alertSubscribers/actions";
+import { setLoading, removeLoading } from "../store/isLoading/actions";
+
+const endpoint = "/alertSubscriber";
+
+export const getAlertSubscribers = () => async (dispatch : any) => {
+    
+    try{
+        dispatch(setLoading());
+        const res =  await RestHTTP.GET(endpoint,[]);
+        dispatch(fetchAlertSubscribers(res))
+        dispatch(removeLoading());
+    }catch (e) {
+        dispatch(removeLoading());
+        throw e;
+    }
+
+}
